a30337d18585c7dc0643e85c52a8dd561a34a8d2
rusta <rustam_ussr@mail.ru>
2023-04-18 16:00:02 +0300

    Initial commit

---
+++ src/main/java/com/job4j/auth/Job4jAuth1Application.java
@@ -0,0 +1,13 @@
+package com.job4j.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jAuth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jAuth1Application.class, args);
+	}
+
+}
