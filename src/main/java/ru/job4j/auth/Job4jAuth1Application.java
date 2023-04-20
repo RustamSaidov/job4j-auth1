d6a5264455c12335010852cafdb50d66310b8331
Qafas <rustam_ussr@mail.ru>
2023-04-21 00:15:29 +0400

    1. Spring Liquibase [#302330 #410151]

--- src/main/java/ru/job4j/auth/Job4jAuth1Application.java
+++ src/main/java/ru/job4j/auth/Job4jAuth1Application.java
@@ -1,13 +1,30 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Job4jAuth1Application {
+public class Job4jAuth1Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAuth1Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Job4jAuth1Application.class);
+    }
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
 }
